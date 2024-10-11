ee011c404439d97fe24746018e791ad3cb336cc2
DanielGT026 <Dgualberto405@gmail.com>
2024-10-11 12:24:07 -0300

    TCC_2.3: Alteração da tela Index/Adição da tela cadastro e candidatura

--- obj/Debug/net8.0/ChronosMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChronosMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChronosMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42e248bcaca8d531871eb1bd082e3c0333645fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bb30e14fec63aff0fab7c53133168da6dc6213")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChronosMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChronosMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
